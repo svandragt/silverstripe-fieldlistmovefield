e16f97e503cb428e969f924dfc934228bb528b91
helpfulrobot <chris@silverstripe.com>
2016-01-01 00:04:18 +1300

    Converted to PSR-2

--- code/FieldListMoveFieldExtension.php
+++ code/FieldListMoveFieldExtension.php
@@ -1,32 +1,34 @@
 <?php
-class FieldListMoveFieldExtension extends Extension {
+class FieldListMoveFieldExtension extends Extension
+{
 
-	public function moveAfter($source, $destination) {
-		list ($field, $field_name) = $this->fieldFromName($source);
+    public function moveAfter($source, $destination)
+    {
+        list($field, $field_name) = $this->fieldFromName($source);
 
-		$this->owner->removeByName($field_name);
-		$this->owner->insertAfter($field,$destination);
-	}
+        $this->owner->removeByName($field_name);
+        $this->owner->insertAfter($field, $destination);
+    }
 
-	public function moveBefore($source, $destination) {
-		list ($field, $field_name) = $this->fieldFromName($source);
-		$this->owner->removeByName($field_name);
-		$this->owner->insertBefore($field,$destination);
-	}
+    public function moveBefore($source, $destination)
+    {
+        list($field, $field_name) = $this->fieldFromName($source);
+        $this->owner->removeByName($field_name);
+        $this->owner->insertBefore($field, $destination);
+    }
 
-	private function fieldFromName($source) {
-		if (is_object($source)) {
-			$field = $source;
-			$field_name = $field->Name;
-		}
-		else {
-			$field_name = $source;
-			$field = $this->owner->fieldByName($source);
-			if (empty($field)) {
-				$field = $this->owner->dataFieldByName($source);
-			}
-		}
-		return array($field, $field_name);
-	}
-
+    private function fieldFromName($source)
+    {
+        if (is_object($source)) {
+            $field = $source;
+            $field_name = $field->Name;
+        } else {
+            $field_name = $source;
+            $field = $this->owner->fieldByName($source);
+            if (empty($field)) {
+                $field = $this->owner->dataFieldByName($source);
+            }
+        }
+        return array($field, $field_name);
+    }
 }
